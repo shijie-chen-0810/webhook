d7f4237562037029dbf673aeb9754946a4b128a4
九磅十五便士 <1131664585@qq.com>
2022-01-09 19:41:25 +0800

    添加bodyparser

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
   console.log("webhook Listen Event");
+  console.log(req.body, "req.bodyreq.body");
   res.end(JSON.stringify({ ok: true }));
 });
 
