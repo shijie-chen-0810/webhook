a8e70b3598ce38b64c2ea186f95dc767796cf39f
九磅十五便士 <1131664585@qq.com>
2022-01-09 19:52:12 +0800

    添加bodyparser

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const SECRET = "88888888";
 const app = express();
+const sign = (body) => {
+  return `sha1=${crypto.createHmac("sha1", SECRET).update(body).digest("hex")}`;
+};
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.post("/webhook", (req, res) => {
   console.log("webhook Listen Event");
   console.log(req.body, "req.bodyreq.body");
+  const event = req.headers["x-gitHub-event"]; // event === push
+  const signnature = req.headers["x-hub-signature"];
+  if (signnature !== sign(req.body)) {
+    return res.end("Not Allowed");
+  }
+  console.log("验证通过");
   res.end(JSON.stringify({ ok: true }));
 });
 
